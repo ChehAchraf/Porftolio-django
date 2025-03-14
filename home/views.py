dd04e0f9d97a5e929ad133a12fc5807b4c4a8b6f
ChehAchraf <achrafchehboun@outlook.com>
2025-03-14 12:44:37 +0000

    add the techno percent

--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from .models import PersonalInfo, TechnichalExpertise, Technology, Project
 
 def home(request):
     personal_info = PersonalInfo.objects.get(id=1)
-    technichal_expertise = TechnichalExpertise.objects.all()
+    front_end_technologies = TechnichalExpertise.objects.filter(category='front-end')
+    back_end_technologies = TechnichalExpertise.objects.filter(category='back-end')
     technology = Technology.objects.all()
     project = Project.objects.all()
-    return render(request, 'home.html' , {'ashraf':personal_info,'technology':technology})
+    return render(request, 'home.html' , {'ashraf':personal_info,'front_end_technologies':front_end_technologies,'back_end_technologies':back_end_technologies,'technology':technology,'project':project})
